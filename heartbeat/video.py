1e198e5161c3c75fef91ef0868e7df3a860f00f9
Matheus Cariús <13924985+matheuscarius@users.noreply.github.com>
2017-04-29 02:30:53 -0300

    Add comments to video

--- heartbeat/video.py
+++ heartbeat/video.py
@@ -1,22 +1,27 @@
 import cv2
 import numpy as np
-import os
 
 def calculate_brightness(video_path):
-    video = cv2.VideoCapture(video_path)
-    FPS = video.get(cv2.CAP_PROP_FPS)
-    brightness = []
+    #Get video
+    video = cv2.VideoCapture(video_path)    
+    #Get video FPS
+    FPS = video.get(cv2.CAP_PROP_FPS)       
+    #Set brightness empty initially
+    brightness = []                         
 
     while(video.isOpened()):
-        ret, frame = video.read()
+        #Get video frame
+        ret, frame = video.read()           
 
-        if ret is False:
+        #Check is there is a frame
+        if ret is False:                    
             break
-
-        brightness.append(np.mean(frame[:,:,2]))
+        
+        #Get mean of red channel as brightness
+        brightness.append(np.mean(frame[:,:,2])
        
+    #Release video
     video.release()
-    cv2.destroyAllWindows()
 
     return brightness, FPS
 
