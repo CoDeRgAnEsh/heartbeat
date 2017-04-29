ce1394cb98e0e120fb2feb93db88a8d5ca911106
Matheus Cariús <matheus.carius37@gmail.com>
2017-04-29 02:40:03 -0300

    Add filtering comments

--- heartbeat/processing.py
+++ heartbeat/processing.py
@@ -4,12 +4,18 @@ import video
 import matplotlib.pyplot as plt
 
 def filter_brightness(brightness, FPS):
+    #Set low and high BPM limits
     BPM_L = 40
     BPM_H = 230
+    #Set filter stabilization time in seconds
     FILTER_STABILIZATION_TIME = 1
+    #Create a digital butterworth filter (frequencies are normalized, 1 corresponds to half the sampling rate)
     b, a = signal.butter(2, [(BPM_L/60)/FPS*2, (BPM_H/60)/FPS*2], 'bandpass')
+    #Apply filter
     filtered = signal.lfilter(b,a,brightness)
+    #Remove initial stabilization time
     filtered = filtered[int(FPS*FILTER_STABILIZATION_TIME+1):filtered.size]
+
     return filtered
 
 if __name__=='__main__':
