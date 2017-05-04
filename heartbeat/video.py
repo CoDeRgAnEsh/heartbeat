c2ba49ee87eb9a94d6552823172712e086480f70
Matheus Cariús <13924985+matheuscarius@users.noreply.github.com>
2017-05-04 16:45:26 -0300

    Add print messages about program progress

--- heartbeat/video.py
+++ heartbeat/video.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 
 def calculate_brightness(video_path):
+
+    print("Opening", video_path);
+
     #Get video
     video = cv2.VideoCapture(video_path)    
     #Get video FPS
@@ -9,9 +12,21 @@ def calculate_brightness(video_path):
     #Set brightness empty initially
     brightness = []                         
 
+    #Calculate progress
+    progress = 10
+    frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    count = 0
+
+    print("Processed {}% of {}".format((10-progress)*10, video_path))
     while(video.isOpened()):
         #Get video frame
         ret, frame = video.read()           
+
+        #Print progress
+        count += 1
+        if(count*progress > frames):
+            progress -= 1
+            print("Processed {}% of {}".format((10-progress)*10, video_path))
 
         #Check is there is a frame
         if ret is False:                    
@@ -20,6 +35,7 @@ def calculate_brightness(video_path):
         #Get mean of red channel as brightness
         brightness.append(np.mean(frame[:,:,2]))
        
+    print("Closing", video_path)
     #Release video
     video.release()
 
