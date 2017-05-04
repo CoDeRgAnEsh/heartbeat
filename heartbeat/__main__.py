64f90fce33e69873b6670c15e470a78ad09c5a6f
Matheus Cariús <13924985+matheuscarius@users.noreply.github.com>
2017-05-04 18:16:06 -0300

    Change main entry point

--- heartbeat/__main__.py
+++ heartbeat/__main__.py
@@ -7,10 +7,7 @@ def main():
         print("Usage: '{} <path to video>'".format(sys.argv[0]))
         return 
 
-    brightness, FPS = video.calculate_brightness(sys.argv[1])
-    filtered_brightness = processing.filter_brightness(brightness, FPS)
-    i = processing.brightness_FFT(filtered_brightness[0:int(6*FPS)])
-    print(i)
+    processing.process_video(sys.argv[1])
 
 if __name__ == '__main__':
     main()
